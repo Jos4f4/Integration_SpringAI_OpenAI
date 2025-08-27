99a1bd6290c868f15c19ed9555452b9c162dfa30
Josaf4 <josafa.soares2909@gmail.com>
2025-08-27 00:51:16 -0400

    Implementation Service and Controller of API in Spring Boot.

---
+++ spring-ai-erudio/src/main/java/br/com/erudio/controller/GenerativeAIController.java
@@ -0,0 +1,28 @@
+package br.com.erudio.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.erudio.service.ChatService;
+
+@RestController
+public class GenerativeAIController {
+
+	private final ChatService service;
+
+	public GenerativeAIController(ChatService service) {
+		this.service = service;
+	}
+	
+	@GetMapping("ask-ai")
+	public String getResponse(@RequestParam String prompt) {
+		return service.getResponse(prompt);
+	}
+	
+	@GetMapping("ask-ai-options")
+	public String getResponseWithOptions(@RequestParam String prompt) {
+		return service.getResponseWithOptions(prompt);
+	}
+}
+
