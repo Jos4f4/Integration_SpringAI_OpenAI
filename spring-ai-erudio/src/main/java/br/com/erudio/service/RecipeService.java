4fe9e474cfdf4df52736d8431fddd972b60e73ce
Josaf4 <josafa.soares2909@gmail.com>
2025-08-28 01:22:47 -0400

    Implemented and test prompt passed int the url of test.

--- spring-ai-erudio/src/main/java/br/com/erudio/service/RecipeService.java
+++ spring-ai-erudio/src/main/java/br/com/erudio/service/RecipeService.java
@@ -20,7 +20,7 @@ public class RecipeService {
 								String cuisine, 
 								String dietaryRestrictions) {
 		var template = """
-				I want to create a recipe using the following ingredients: {ingredientes}
+				I want to create a recipe using the following ingredients: {ingredients}
 				The cuisine type I prefer is {cuisine}.
 				Please consider the following dietary restrictions: {dietaryRestrictions}.
 				Please provide me with a detailed recipe including title, list of ingredients, and cooking instructions
