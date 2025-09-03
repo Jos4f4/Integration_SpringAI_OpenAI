03037091ed04c7d6c984977275a78aca0960db79
Josaf4 <josafa.soares2909@gmail.com>
2025-09-03 00:14:17 -0400

    Implementation the frontend

---
+++ frontend/api-client/src/App.js
@@ -0,0 +1,22 @@
+import { useState  } from 'react';
+import './App.css';
+
+function App() {
+
+  const [activeTab, setActiveTab] = useState('ask-ai')
+
+  const handleTabChange = (tab) => {
+    alert(tab);
+    setActiveTab(tab);
+  }
+
+  return (
+    <div className="App">
+      <button onClick={ () => handleTabChange('ask-ai')}>Talk with AI</button>
+      <button onClick={ () => handleTabChange('recipe-generator')}>Generate Recipes</button>
+      <button onClick={ () => handleTabChange('image-generator')}>Generate Images</button>
+    </div>
+  );
+}
+
+export default App;
