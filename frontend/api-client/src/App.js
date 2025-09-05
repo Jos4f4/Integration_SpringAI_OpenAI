aa0efa322acd6ef0e911b72b7ccb544c6160b751
Josaf4 <josafa.soares2909@gmail.com>
2025-09-05 01:10:30 -0400

    Implemented new features in the screen of client.

--- frontend/api-client/src/App.js
+++ frontend/api-client/src/App.js
@@ -1,20 +1,41 @@
 import { useState  } from 'react';
 import './App.css';
+import TalWithAI from './pages/chat/TalkWithAI';
+import RecipeGenerator from './pages/recipe/RecipeGenerator';
+import ImageGenerator from './pages/image/ImageGenerator';
 
 function App() {
 
   const [activeTab, setActiveTab] = useState('ask-ai')
 
   const handleTabChange = (tab) => {
-    alert(tab);
+    //alert(tab);
     setActiveTab(tab);
   }
 
   return (
     <div className="App">
-      <button onClick={ () => handleTabChange('ask-ai')}>Talk with AI</button>
-      <button onClick={ () => handleTabChange('recipe-generator')}>Generate Recipes</button>
-      <button onClick={ () => handleTabChange('image-generator')}>Generate Images</button>
+      <button 
+        className={activeTab === 'ask-ai' ? 'active' : ''}
+        onClick={ () => handleTabChange('ask-ai')}>
+          Talk with AI
+      </button>
+      <button 
+        className={activeTab === 'recipe-generator' ? 'active' : ''}
+        onClick={ () => handleTabChange('recipe-generator')}>
+          Generate Recipes
+      </button>
+      <button 
+        className={activeTab === 'image-generator' ? 'active' : ''}
+        onClick={ () => handleTabChange('image-generator')}>
+         Generate Images
+      </button>
+      <div>
+          {activeTab === 'ask-ai' && <TalWithAI/>}
+          {activeTab === 'recipe-generator' &&  <RecipeGenerator/>}
+          {activeTab === 'image-generator' && <ImageGenerator/>}
+      </div>
+
     </div>
   );
 }
